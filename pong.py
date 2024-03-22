d505e9e72725fdfae99512c8770eb9647cf9f30c
Jesus Galaz <jesusgalazr@icloud.com>
2024-03-22 09:57:25 -0600

    Agregando funcionalidad de nueva bola al picarle al espacio

--- pong.py
+++ pong.py
@@ -1,6 +1,5 @@
 from random import choice, random
 from turtle import *
-
 from freegames import vector
 
 bgcolor('black')
@@ -8,12 +7,11 @@ bgcolor('black')
 def value():
     return (3 + random() * 2) * choice([1, -1])
 
-balls = [(vector(0, 0), vector(value(), value()))]  # Correction here
+balls = [(vector(0, 0), vector(value(), value()))]
 state = {1: 0, 2: 0}
 score = {1: 0, 2: 0}
-speed_increment = .5  
-current_speed = 1.0  
-
+speed_increment = .5
+current_speed = 1.0
 is_paused = False
 
 def move(player, change):
@@ -33,42 +31,34 @@ def rectangle(x, y, width, height):
 
 def draw():
     global current_speed
-
     if not is_paused:  # Only draw and move if not paused
         clear()
-
         color('white')
         rectangle(-200, state[1], 10, 50)
         rectangle(190, state[2], 10, 50)
-
         for ball, aim in balls:
             up()
             goto(ball.x, ball.y)
             dot(10, "white")
-
-            ball.move(aim * current_speed) 
+            ball.move(aim * current_speed)
             x = ball.x
             y = ball.y
-
             if y < -200 or y > 200:
                 aim.y = -aim.y
-
             if x < -185:
                 if state[1] <= y <= state[1] + 50:
                     aim.x = -aim.x
-                else: 
-                    score[2] += 1  # Note the change here
+                else:
+                    score[2] += 1
                     aim.x = -aim.x  # Bounce off the wall
             if x > 185:
                 if state[2] <= y <= state[2] + 50:
                     aim.x = -aim.x
                 else:
-                    score[1] += 1  # Note the change here
+                    score[1] += 1
                     aim.x = -aim.x  # Bounce off the wall
-
-        draw_score()  
+        draw_score()
         update()
-
     ontimer(draw, 50)
 
 def draw_score():
@@ -85,6 +75,9 @@ def toggle_pause():  # Create a function to toggle the pause state
     global is_paused
     is_paused = not is_paused
 
+def add_ball():
+    balls.append((vector(0, 0), vector(value(), value())))
+
 setup(420, 420, 370, 0)
 hideturtle()
 tracer(False)
@@ -94,5 +87,6 @@ onkey(lambda: move(1, -20), 's')
 onkey(lambda: move(2, 20), 'i')
 onkey(lambda: move(2, -20), 'k')
 onkey(toggle_pause, 'p')  # Use 'p' key to pause the game
+onkey(add_ball, 'space')  # Use 'space' key to add a new ball
 draw()
 done()
